e527c0d2ac0ef47d3f7b154bcf7e3383561402e5
Liudmyla Basova <basovaln1@gmail.com>
2025-08-12 12:30:21 +0900

    final version 1

---
+++ src/main/java/habsida/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,85 @@
+package habsida.spring.boot_security.demo.controller;
+
+
+import habsida.spring.boot_security.demo.model.User;
+import habsida.spring.boot_security.demo.repository.RoleRepository;
+import habsida.spring.boot_security.demo.service.UserService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.NoSuchElementException;
+
+@Controller
+@RequestMapping("/admin")
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminController {
+
+    private final UserService userService;
+    private final RoleRepository roleRepository;
+
+    public AdminController(UserService userService, RoleRepository roleRepository) {
+        this.userService = userService;
+        this.roleRepository = roleRepository;
+    }
+
+    @GetMapping()
+    public String showAllUsers(Principal principal,Model model) {
+        User user = userService.findByUsername(principal.getName());
+
+        model.addAttribute("users", userService.getAllUsers());
+        model.addAttribute("authName", user.getUsername() + " || " + user.getEmail());
+        model.addAttribute("isAdmin", true);
+        return "admin/list";
+    }
+
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("allRoles", roleRepository.findAll());
+        return "/admin/add";
+    }
+
+    @PostMapping("/save")
+    public String saveUser(@ModelAttribute("user") User user, Model model) {
+        try {
+            userService.saveUser(user);
+            return "redirect:/admin";
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", e.getMessage());
+            return "/admin/add";
+        }
+    }
+
+    @GetMapping("/delete")
+    public String deleteUser(@RequestParam("id") Long id,Model model) {
+        try {
+            userService.deleteUserById(id);
+        } catch (NoSuchElementException e){
+            model.addAttribute("error", e.getMessage());
+        }
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/edit")
+    public String showEditForm(@RequestParam("id") Long id, Model model) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("allRoles", roleRepository.findAll());
+        return "/admin/edit";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") User user, Model model) {
+        try {
+            userService.updateUser(user);
+            return "redirect:/admin";
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", e.getMessage());
+            return "/admin/edit";
+        }
+    }
+
+}
---
+++ src/main/java/habsida/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,19 @@
+package habsida.spring.boot_security.demo.service;
+
+
+import habsida.spring.boot_security.demo.model.User;
+
+public interface UserService {
+    Object getAllUsers();
+
+    void saveUser(User user);
+
+    void deleteUserById(Long id);
+
+    User getUserById(Long id);
+
+    void updateUser(User user);
+
+    User findByUsername(String username);
+
+    }
